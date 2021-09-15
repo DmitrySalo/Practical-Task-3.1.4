a3764ff5f9c80b640919cb992ff5b3adf546bed4
Dmitry Salo <morozavros@gmail.com>
2021-09-15 19:08:26 +0500

    Add solution 3.1.4

---
+++ src/main/java/com/example/securityquestion/SecurityquestionApplication.java
@@ -0,0 +1,13 @@
+package com.example.securityquestion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityquestionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecurityquestionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/securityquestion/SecurityquestionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.securityquestion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityquestionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
